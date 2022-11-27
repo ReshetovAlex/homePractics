85ee828b338d20aabca81c534c904dce9302896c
Alex Reshetov <reshetov_alex@mail.ru>
2022-11-27 22:56:58 +0300

    new project Unit

---
+++ src/test/java/homework2/biletTest.java
@@ -0,0 +1,51 @@
+package homework2;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+
+public class biletTest {
+
+    @Test
+  public void getBilet(int x) {
+       x = 100100;
+       boolean chastBilet = true;
+       assertTrue(chastBilet);
+
+   }
+}
+//    private static boolean getResult () {
+//        return true;
+
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//public class sumChiselTest {
+//
+////@BeforeEach
+////public void setUp() {
+//
+//
+//   @Test
+//  public void sumTest() {
+//       int x = 4;
+//       int y = 5;
+//    int result = 9;
+//   assertEquals(result,sumChisel.getsum(x, y));
+//
+//   }
+//
+//    @Test
+//    public void sumTest2() {
+//        int x = 10;
+//        int y = 10;
+//        int result = 15;
+//        assertEquals(result,sumChisel.getsum(x, y));
+//
+//    }
+//}
